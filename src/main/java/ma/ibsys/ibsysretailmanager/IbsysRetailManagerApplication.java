cc7c70d6b0123162c00d8129b0097edbebd7c63e
Youssef Faik <faik.yusef@gmail.com>
2023-04-11 15:48:41 +0000

    initial commit

---
+++ src/main/java/ma/ibsys/ibsysretailmanager/IbsysRetailManagerApplication.java
@@ -0,0 +1,13 @@
+package ma.ibsys.ibsysretailmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbsysRetailManagerApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(IbsysRetailManagerApplication.class, args);
+  }
+  
+}
---
+++ src/test/java/ma/ibsys/ibsysretailmanager/IbsysRetailManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ibsys.ibsysretailmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbsysRetailManagerApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
