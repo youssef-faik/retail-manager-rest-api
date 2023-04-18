fe23553ac94c580b28e84069a6b7cf7edf9d656b
Youssef Faik <64314025+yusef-faik@users.noreply.github.com>
2023-04-18 15:08:51 +0000

    Update IbsysRetailManagerApplicationTests.java

--- src/test/java/ma/ibsys/ibsysretailmanager/IbsysRetailManagerApplicationTests.java
+++ src/test/java/ma/ibsys/ibsysretailmanager/IbsysRetailManagerApplicationTests.java
@@ -1,6 +1,7 @@
 package ma.ibsys.ibsysretailmanager;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +9,7 @@ class IbsysRetailManagerApplicationTests {
   
   @Test
   void contextLoads() {
+    fail("failed error");
   }
   
 }
