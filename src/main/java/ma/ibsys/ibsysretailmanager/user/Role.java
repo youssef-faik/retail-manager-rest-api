6c4b0d7e7d69b6d4911e8ab1e66fe7bfa7bb1406
Youssef Faik <faik.yusef@gmail.com>
2023-04-26 00:46:54 +0100

    add @Schema annotation to the Role enum

--- src/main/java/ma/ibsys/ibsysretailmanager/user/Role.java
+++ src/main/java/ma/ibsys/ibsysretailmanager/user/Role.java
@@ -1,7 +1,13 @@
 package ma.ibsys.ibsysretailmanager.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "The user role")
 public enum Role {
+  @Schema(description = "The administrator role")
   ADMIN,
+  @Schema(description = "The manager role")
   MANAGER,
+  @Schema(description = "The cashier role")
   CASHIER
 }
