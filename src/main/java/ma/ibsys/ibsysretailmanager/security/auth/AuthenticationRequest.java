c0a7a3f0969c34d102029825cd6fe1df2d054ab6
Youssef Faik <faik.yusef@gmail.com>
2023-04-26 00:29:28 +0100

    add @Schema annotation to AuthenticationResponse and AuthenticationRequest

--- src/main/java/ma/ibsys/ibsysretailmanager/security/auth/AuthenticationRequest.java
+++ src/main/java/ma/ibsys/ibsysretailmanager/security/auth/AuthenticationRequest.java
@@ -1,9 +1,8 @@
 package ma.ibsys.ibsysretailmanager.security.auth;
 
-import jakarta.persistence.Column;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,11 +12,16 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(
+    title = "Authentication Request Schema",
+    description = "Request body for user authentication")
 public class AuthenticationRequest {
+  @Schema(description = "User email", example = "yusef@mail.com")
   @Email(message = "email should be valid")
   @NotBlank(message = "email is mandatory")
   private String email;
-  
+
+  @Schema(description = "User password", example = "secret-password")
   @NotBlank(message = "password is mandatory")
   private String password;
 }
--- src/main/java/ma/ibsys/ibsysretailmanager/security/auth/AuthenticationResponse.java
+++ src/main/java/ma/ibsys/ibsysretailmanager/security/auth/AuthenticationResponse.java
@@ -1,5 +1,6 @@
 package ma.ibsys.ibsysretailmanager.security.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,12 +10,23 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(title = "Authentication Response Schema", description = "Response body for user authentication response")
 public class AuthenticationResponse {
+  @Schema(description = "JWT token", example = "eyJhbGciOiJIUzI1NiJ9...")
   private String token;
+
+  @Schema(description = "User ID", example = "1")
   private Long id;
+
+  @Schema(description = "First name", example = "Youssef")
   private String firstName;
+
+  @Schema(description = "Last name", example = "Faik")
   private String lastName;
+
+  @Schema(description = "Email", example = "yusef@mail.com")
   private String email;
+
+  @Schema(description = "User role", example = "ADMIN")
   private String role;
-  
 }
