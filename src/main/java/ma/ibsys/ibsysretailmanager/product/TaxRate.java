18de0484fdca2216212c7de01a1a146ebe28efa1
Youssef Faik <faik.yusef@gmail.com>
2023-04-25 14:37:03 +0100

    associate each enum value with a corresponding tax rate value in TaxRate enum

--- src/main/java/ma/ibsys/ibsysretailmanager/product/TaxRate.java
+++ src/main/java/ma/ibsys/ibsysretailmanager/product/TaxRate.java
@@ -1,14 +1,18 @@
 package ma.ibsys.ibsysretailmanager.product;
 
-/*
-+ STANDARD=20
-+ INTERMEDIATE=14
-+ REDUCED=10
-+ SUPER_REDUCED=7
-*/
 public enum TaxRate {
-  TAX_20,
-  TAX_14,
-  TAX_10,
-  TAX_7
+  TWENTY(20),
+  FOURTEEN(14),
+  TEN(10),
+  SEVEN(7);
+
+  private final int value;
+
+  TaxRate(int value) {
+    this.value = value;
+  }
+
+  public int getValue() {
+    return value;
+  }
 }
