d3c73ac2369a0ac1c7ef9c495ddc5630942a8fe2
Youssef Faik <faik.yusef@gmail.com>
2023-04-25 23:26:09 +0100

    add more Swagger annotations to ProductApi to better describe the Api

--- src/main/java/ma/ibsys/ibsysretailmanager/product/ProductApi.java
+++ src/main/java/ma/ibsys/ibsysretailmanager/product/ProductApi.java
@@ -1,9 +1,15 @@
 package ma.ibsys.ibsysretailmanager.product;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
+import ma.ibsys.ibsysretailmanager.handlers.ErrorResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,29 +23,200 @@ public interface ProductApi {
   @Operation(
       summary = "Get all products",
       description = "Get a list that contains the details for all products.")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Successfully Retrieved the list of all products",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ProductResponseDto.class))),
+        @ApiResponse(
+            responseCode = "400",
+            description = "Bad Request. Invalid input parameters.",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponse.class))),
+        @ApiResponse(
+            responseCode = "401",
+            description = "Unauthorized. Authentication credentials are missing or invalid.",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponse.class))),
+        @ApiResponse(
+            responseCode = "500",
+            description = "Internal Server Error. An error occurred while processing the request.",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponse.class)))
+      })
   ResponseEntity<List<ProductResponseDto>> getAllCustomers();
 
   @GetMapping("/{id}")
   @Operation(
       summary = "Get product details",
       description = "Get the details of the product with the given id.")
-  ResponseEntity<ProductResponseDto> getProduct(@PathVariable("id") int id);
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Product details",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ProductResponseDto.class))),
+        @ApiResponse(
+            responseCode = "400",
+            description = "Bad Request. Invalid input parameters.",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponse.class))),
+        @ApiResponse(
+            responseCode = "401",
+            description = "Unauthorized. Authentication credentials are missing or invalid.",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponse.class))),
+        @ApiResponse(
+            responseCode = "500",
+            description = "Internal Server Error. An error occurred while processing the request.",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponse.class)))
+      })
+  ResponseEntity<ProductResponseDto> getProduct(
+      @Parameter(description = "The id of the product to retrieve", required = true, example = "1")
+          @PathVariable("id")
+          int id);
 
   @PostMapping
   @Operation(
       summary = "Create product",
       description = "Create a new product with the given details.")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "201",
+            description = "Product created successfully.",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ProductResponseDto.class))),
+        @ApiResponse(
+            responseCode = "400",
+            description = "Bad Request. Invalid input parameters.",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponse.class))),
+        @ApiResponse(
+            responseCode = "401",
+            description = "Unauthorized. Authentication credentials are missing or invalid.",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponse.class))),
+        @ApiResponse(
+            responseCode = "500",
+            description = "Internal Server Error. An error occurred while processing the request.",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponse.class)))
+      })
   ResponseEntity<ProductResponseDto> createProduct(
-      @RequestBody ProductRequestDto productRequestDto);
+      @Parameter(
+              description = "Product details",
+              required = true,
+              schema = @Schema(implementation = ProductRequestDto.class))
+          @RequestBody
+          ProductRequestDto productRequestDto);
 
-  @PutMapping("/{id}")
   @Operation(
       summary = "Update product details",
       description = "Update the details of the product with given id.")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Product updated successfully",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ProductResponseDto.class))),
+        @ApiResponse(
+            responseCode = "400",
+            description = "Bad Request. Invalid input parameters.",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponse.class))),
+        @ApiResponse(
+            responseCode = "401",
+            description = "Unauthorized. Authentication credentials are missing or invalid.",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponse.class))),
+        @ApiResponse(
+            responseCode = "500",
+            description = "Internal Server Error. An error occurred while processing the request.",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponse.class)))
+      })
+  @PutMapping("/{id}")
   ResponseEntity<ProductResponseDto> updateProduct(
-      @PathVariable("id") int id, @RequestBody ProductRequestDto productRequestDto);
+      @Parameter(description = "The id of the product to update", required = true, example = "1")
+          @PathVariable("id")
+          int id,
+      @Parameter(
+              description = "The object containing the new details of the product to update.",
+              required = true,
+              schema = @Schema(implementation = ProductRequestDto.class))
+          @RequestBody
+          ProductRequestDto productRequestDto);
 
-  @DeleteMapping("/{id}")
   @Operation(summary = "Delete product", description = "Delete a product with given id.")
-  ResponseEntity<Void> deleteProduct(@PathVariable("id") int id);
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "204",
+            description = "Product deleted successfully.",
+            content = @Content(mediaType = "application/json")),
+        @ApiResponse(
+            responseCode = "400",
+            description = "Bad Request. Invalid input parameters.",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponse.class))),
+        @ApiResponse(
+            responseCode = "401",
+            description = "Unauthorized. Authentication credentials are missing or invalid.",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponse.class))),
+        @ApiResponse(
+            responseCode = "500",
+            description = "Internal Server Error. An error occurred while processing the request.",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponse.class)))
+      })
+  @DeleteMapping("/{id}")
+  ResponseEntity<Void> deleteProduct(
+      @Parameter(name = "id", description = "The id of the product to delete", required = true, example = "1")
+          @PathVariable("id")
+          int id);
 }
