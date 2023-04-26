a491efe9482330fa38af23499b97f741711327d8
Youssef Faik <faik.yusef@gmail.com>
2023-04-26 13:14:55 +0100

    add @Table and @Column annotations for the Token entity

--- src/main/java/ma/ibsys/ibsysretailmanager/security/token/Token.java
+++ src/main/java/ma/ibsys/ibsysretailmanager/security/token/Token.java
@@ -1,14 +1,6 @@
 package ma.ibsys.ibsysretailmanager.security.token;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,24 +11,35 @@ import ma.ibsys.ibsysretailmanager.user.User;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
+@Entity(name = "Token")
+@Table(
+    name = "token",
+    uniqueConstraints = {@UniqueConstraint(name = "token_token_unique", columnNames = "token")})
 public class Token {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", updatable = false)
   public Long id;
 
-  @Column(unique = true)
+  @Column(name = "token", updatable = false)
   public String token;
 
   @Enumerated(EnumType.STRING)
+  @Column(name = "token_type", nullable = false)
   public TokenType tokenType = TokenType.BEARER;
 
+  @Column(name = "revoked", nullable = false)
   public boolean revoked;
 
+  @Column(name = "expired", nullable = false)
   public boolean expired;
 
   @ManyToOne
-  @JoinColumn(name = "user_id")
+  @JoinColumn(
+      name = "user_id",
+      referencedColumnName = "id",
+      nullable = false,
+      foreignKey = @ForeignKey(name = "token_user_id_fk"))
   public User user;
 }
