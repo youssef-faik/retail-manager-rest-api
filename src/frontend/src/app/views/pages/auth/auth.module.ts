33977131ea2fb49265b70ac9a6ecfed5d88b71f2
Youssef Faik <faik.yusef@gmail.com>
2023-05-01 19:53:36 +0100

    delete the RegisterComponent + clean the LoginComponent

--- src/frontend/src/app/views/pages/auth/auth.module.ts
+++ src/frontend/src/app/views/pages/auth/auth.module.ts
@@ -1,9 +1,8 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { Routes, RouterModule } from '@angular/router';
-import { AuthComponent } from './auth.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LoginComponent} from './login/login.component';
+import {RouterModule, Routes} from '@angular/router';
+import {AuthComponent} from './auth.component';
 
 const routes: Routes = [
   {
@@ -18,20 +17,17 @@ const routes: Routes = [
       {
         path: 'login',
         component: LoginComponent
-      },
-      {
-        path: 'register',
-        component: RegisterComponent
       }
     ]
   },
 ]
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent, AuthComponent],
+  declarations: [LoginComponent, AuthComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
   ]
 })
-export class AuthModule { }
+export class AuthModule {
+}
